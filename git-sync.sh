f62f8adef21f3713eb955aa1efa9f37bc95c5404
Robert LaRocca <29333852+robertlarocca@users.noreply.github.com>
2023-07-15 15:27:53 -0400

    Add `git-sync.sh` and `theme-helper.sh`. - Include support for new scripts.

---
+++ git-sync.sh
@@ -0,0 +1,177 @@
+#!/bin/bash
+
+# Copyright (c) 2023 Robert LaRocca <robert@laroccx.com>
+
+# Synchronize all Git repositories in the current directory or the list of directories.
+
+# Script version and release
+script_version='1.0.0'
+script_release='devel'  # options devel, beta, release, stable
+
+# Uncomment to enable bash xtrace mode.
+set -xv
+
+require_root_privileges() {
+	if [[ "$(whoami)" != "root" ]]; then
+		# logger -i "Error: git-sync must be run as root!"
+		echo "Error: git-sync must be run as root!" >&2
+		exit 2
+	fi
+}
+
+require_user_privileges() {
+	if [[ "$(whoami)" == "root" ]]; then
+		# logger -i "Error: git-sync must be run as normal user!"
+		echo "Error: git-sync must be run as normal user!" >&2
+		exit 2
+	fi
+}
+
+show_help_message() {
+	cat <<-EOF_XYZ
+	Usage: git-sync [OPTION] [URI]...
+	Easily synchronize cloned Git repositories on the local filesystem
+	or forked repositories with upstream on the internet. By default the
+	current directory is synchronized unless another directory path or
+	option is provided.
+
+	Options:
+	 all - synchronize all repositories in configuration list
+	 url - synchronize and merge upstream repository
+
+	 version - show version information
+	 help - show this help message
+
+	Examples:
+	 git-sync
+	 git-sync --all
+	 git-sync /path/to/repos/
+	 git-sync --url git@example.com/project-repo.git
+	 git-sync upstream https://example.com/project-repo.git
+
+	Exit status:
+	 0 - ok
+	 1 - minor issue
+	 2 - serious error
+
+	Copyright (c) $(date +%Y) Robert LaRocca, https://www.laroccx.com
+	License: The MIT License (MIT)
+	Source: https://github.com/robertlarocca/helpful-linux-bash-scripts
+
+	See git(1) git-pull(1) git-fetch(1) git-push(1) and gittutorial(7) for
+	additonal information and to provide insight how this wrapper works.
+	EOF_XYZ
+}
+
+show_version_information() {
+	cat <<-EOF_XYZ
+	git-sync $script_version-$script_release
+	Copyright (c) $(date +%Y) Robert LaRocca, https://www.laroccx.com
+	License: The MIT License (MIT)
+	Source: https://github.com/robertlarocca/helpful-linux-bash-scripts
+	EOF_XYZ
+}
+
+error_unrecognized_option() {
+	cat <<-EOF_XYZ
+	git-sync: unrecognized option '$1'
+	Try 'git-sync --help' for more information.
+	EOF_XYZ
+}
+
+check_binary_exists() {
+	local binary_command="$1"
+	if [[ ! -x "$(which $binary_command)" ]]; then
+		if [[ -x "/lib/command-not-found" ]]; then
+			/lib/command-not-found "$binary_command"
+		else
+			cat <<-EOF_XYZ >&2
+			Command '$binary_command' not found, but might be installed with:
+			apt install "$binary_command"   # or
+			dnf install "$binary_command"   # or
+			opkg install "$binary_command"  # or
+			snap install "$binary_command"  # or
+			yum install "$binary_command"
+			See your Linux documentation for which 'package manager' to use.
+			EOF_XYZ
+		fi
+		exit 1
+	fi
+}
+
+sync_directory() {
+	if [[ -z "$1" ]]; then
+		local starting_directory="$PWD"
+	elif [[ -n "$1" ]] && [[ -d "$1" ]]; then
+		local starting_directory="$PWD"
+
+		cd "$1"
+	fi
+
+	for i in $(ls -1); do
+		if [[ -s "$i/.git/config" ]]; then
+			cd "$i"
+			echo "Synchronizing $(basename $i)..."
+			git pull
+			git fetch --all
+			git push
+			echo
+			cd ..
+		fi
+	done
+
+	cd "$starting_directory"
+}
+
+sync_upstream() {
+	if [[ -z "$1" ]]; then
+		echo "Error: Must provide URL to upstream git repository!" >&2
+		exit 2
+	fi
+
+	remote add upstream "$1"
+	git remote
+	git fetch upstream
+	git checkout master
+	git merge upstream/master
+	git push origin master
+}
+
+sync_list() {
+	if [[ -s "/etc/git-sync.conf" ]]; then
+		git_sync_config="/etc/git-sync.conf"
+	elif [[ -s "$HOME/.git-sync" ]]; then
+		git_sync_config="$HOME/.git-sync"
+	else
+		echo "Error: git-sync configuration is unavailable!" >&2
+		exit 2
+	fi
+
+	for i in $(grep -v -E '^#|^;|^ ' "$git_sync_config"); do
+		sync_directory "$i"
+	done
+}
+
+check_binary_exists git
+
+case "$1" in
+all)
+	sync_list
+	;;
+url)
+	sync_upstream "$2"
+	;;
+version)
+	show_version_information
+	;;
+help | --help)
+	show_help_message
+	;;
+*)
+	sync_directory "$1"
+	;;
+esac
+
+exit 0
+
+# vi: syntax=sh ts=2 noexpandtab
---
+++ theme-helper.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+
+# Copyright (c) 2023 Robert LaRocca <robert@laroccx.com>
+
+# Customize the GNOME desktop enviroment theme and application settings.
+
+# Script version and release
+script_version='1.0.0'
+script_release='devel'  # options devel, beta, release, stable
+
+require_root_privileges() {
+	if [[ "$(whoami)" != "root" ]]; then
+		# logger -i "Error: theme-helper must be run as root!"
+		echo "Error: theme-helper must be run as root!" >&2
+		exit 2
+	fi
+}
+
+require_user_privileges() {
+	if [[ "$(whoami)" == "root" ]]; then
+		# logger -i "Error: theme-helper must be run as normal user!"
+		echo "Error: theme-helper must be run as normal user!" >&2
+		exit 2
+	fi
+}
+
+show_help_message() {
+	cat <<-EOF_XYZ
+	Usage: theme-helper [OPTION]...
+	Easily customize the GNOME desktop enviroment theme and application
+	settings. This script also configures apt (dpkg) post-invoke to run
+	after installing and upgrading packages.
+
+	Options:
+	 all - configure and customize applications, auto invoke theme-helper
+	 auto - invoke theme-helper after apt packages are installed
+	 config - configure GNOME desktop and GTK settings
+	 custom - customize select applications exec, icon, name etc
+	 hide - hide select applications
+
+	 version - show version information
+	 help - show this help message
+
+	When using theme-helper 'Yaru-Dark' is the default appearance style
+	and 'fingers' is the default click-method for touchpad peripherals.
+
+	Exit status:
+	 0 - ok
+	 1 - minor issue
+	 2 - serious error
+
+	Copyright (c) $(date +%Y) Robert LaRocca, https://www.laroccx.com
+	License: The MIT License (MIT)
+	Source: https://github.com/robertlarocca/helpful-linux-bash-scripts
+
+	See apt(8) apt-config(8) dpkg(1) dpkg-reconfigure(8) for additonal
+	information and to provide insight how this wrapper works.
+	EOF_XYZ
+}
+
+show_version_information() {
+	cat <<-EOF_XYZ
+	theme-helper $script_version-$script_release
+	Copyright (c) $(date +%Y) Robert LaRocca, https://www.laroccx.com
+	License: The MIT License (MIT)
+	Source: https://github.com/robertlarocca/helpful-linux-bash-scripts
+	EOF_XYZ
+}
+
+error_unrecognized_option() {
+	cat <<-EOF_XYZ
+	theme-helper: unrecognized option '$1'
+	Try 'theme-helper --help' for more information.
+	EOF_XYZ
+}
+
+configure_dpkg_invoke() {
+	if [[ ! -f "/etc/apt/apt.conf.d/80theme-helper" ]]; then
+		cat <<-EOF_XYZ > /etc/apt/apt.conf.d/80theme-helper
+		# Customize the GNOME desktop enviroment theme and application settings.
+		DPkg::Post-Invoke {"$(which $0) 2> /dev/null";};
+		EOF_XYZ
+	fi
+}
+
+configure_gsettings() {
+	if [[ -x $(which gsettings) ]]; then
+		gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark' 2> /dev/null
+		gsettings set org.gnome.desktop.peripherals.touchpad click-method 'fingers' 2> /dev/null
+	fi
+}
+
+customize_applications() {
+	sed -E -i s/'Name=Microsoft Edge'/'Name=Browser'/g /usr/share/applications/microsoft-edge.desktop
+	sed -E -i s/'^Icon=.*'/'Icon=webbrowser-app'/g /usr/share/applications/microsoft-edge.desktop
+	sed -E -i s/'Exec=\/usr\/bin\/microsoft-edge-stable'/'Exec=\/usr\/bin\/microsoft-edge-stable --force-dark-mode'/g /usr/share/applications/microsoft-edge.desktop
+
+	# sed -E -i s/'Name=Microsoft Teams'/'Name=Teams'/g /usr/share/applications/teams.desktop
+	# sed -E -i s/'^Icon=.*'/'Icon=camera-app'/g /usr/share/applications/teams.desktop
+
+	sed -E -i s/'Name=Microsoft Teams'/'Name=Teams'/g /home/robert/.local/share/applications/msedge-cifhbcnohmdccbgoicgdjpfamggdegmo-Default.desktop
+	sed -E -i s/'^Icon=.*'/'Icon=camera-app'/g /home/robert/.local/share/applications/msedge-cifhbcnohmdccbgoicgdjpfamggdegmo-Default.desktop
+
+	sed -E -i s/'Name=Visual Studio Code'/'Name=Code'/g /usr/share/applications/code.desktop
+	sed -E -i s/'^Icon=.*'/'Icon=accessories-text-editor'/g /usr/share/applications/code.desktop
+
+	sed -E -i s/'Name=Remmina'/'Name=Remmina'/g /usr/share/applications/org.remmina.Remmina.desktop
+	sed -E -i s/'^Icon=.*'/'Icon=org.remmina.Remmina'/g /usr/share/applications/org.remmina.Remmina.desktop
+
+	sed -E -i s/'Name=Geary'/'Name=Mail'/g /usr/share/applications/org.gnome.Geary.desktop
+	sed -E -i s/'^Icon=.*'/'Icon=mail-app'/g /usr/share/applications/org.gnome.Geary.desktop
+
+	sed -E -i s/'Name=Rhythmbox'/'Name=Music'/g /usr/share/applications/org.gnome.Rhythmbox3.desktop
+	sed -E -i s/'Name=Rhythmbox'/'Name=Music'/g /usr/share/applications/org.gnome.Rhythmbox3.device.desktop
+	sed -E -i s/'^Icon=.*'/'Icon=rhythmbox'/g /usr/share/applications/org.gnome.Rhythmbox3.desktop
+	sed -E -i s/'^Icon=.*'/'Icon=rhythmbox'/g /usr/share/applications/org.gnome.Rhythmbox3.device.desktop
+
+	sed -E -i s/'Name=Disk Usage Analyzer'/'Name=Disk Usage'/g /usr/share/applications/org.gnome.baobab.desktop
+	sed -E -i s/'Name=Disks'/'Name=Disk Utility'/g /usr/share/applications/org.gnome.DiskUtility.desktop
+	sed -E -i s/'Name=Passwords and Keys'/'Name=Keyring'/g /usr/share/applications/org.gnome.seahorse.Application.desktop
+	sed -E -i s/'Name=Power Statistics'/'Name=Power Stats'/g /usr/share/applications/org.gnome.PowerStats.desktop
+	sed -E -i s/'Name=Startup Applications'/'Name=Startup Apps'/g /usr/share/applications/gnome-session-properties.desktop
+	sed -E -i s/'Name=Ubuntu Software'/'Name=Software'/g /var/lib/snapd/desktop/applications/snap-store_ubuntu-software.desktop
+}
+
+hide_applications() {
+	for i in \
+		/usr/share/applications/byobu.desktop \
+		/usr/share/applications/gnome-language-selector.desktop \
+		/usr/share/applications/htop.desktop \
+		/usr/share/applications/im-config.desktop \
+		/usr/share/applications/info.desktop \
+		/usr/share/applications/nm-connection-editor.desktop \
+		/usr/share/applications/org.gnome.eog.desktop \
+		/usr/share/applications/org.gnome.Evince.desktop \
+		/usr/share/applications/software-properties-drivers.desktop \
+		/usr/share/applications/software-properties-gtk.desktop \
+		/usr/share/applications/update-manager.desktop \
+		/usr/share/applications/vim.desktop \
+		; do
+
+		if [[ -s "$i" ]]; then
+			if [[ "$(grep --count 'NoDisplay=true' "$i" 2> /dev/null)" == "0" ]]; then
+				echo 'NoDisplay=true' >> "$i"
+			fi
+		fi
+	done
+}
+
+# Options
+case "$1" in
+all)
+	configure_logrotate
+	configure_dpkg_invoke
+	configure_gsettings
+	customize_applications
+	hide_applications
+	;;
+auto)
+	configure_logrotate
+	configure_dpkg_invoke
+	;;
+config)
+	configure_gsettings
+	;;
+custom)
+	customize_applications
+	;;
+hide)
+	hide_applications
+	;;
+version)
+	show_version_information
+	;;
+help | --help)
+	show_help_message
+	;;
+*)
+	if [[ -z "$1" ]]; then
+	configure_gsettings
+	customize_applications
+	hide_applications
+	else
+		error_unrecognized_option "$*"
+		exit 1
+	fi
+	;;
+esac
+
+remove_pid_lock
+exit 0
+
+# vi: syntax=sh ts=2 noexpandtab
