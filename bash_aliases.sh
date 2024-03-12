c5b977bf7140f3269431ce2028dd13351e9c5ee5
Robert LaRocca <29333852+robertlarocca@users.noreply.github.com>
2024-03-11 22:14:27 -0400

    Bump version to 2.9.1-stable
    - Add bash_aliases fuction with options to show help message and version information.
    - Add alias to better support the clean scripts intended behavior.
    - Change adpasswd to use the local domain_name variable as default domain_controller.
    - Change the emojis added to user PS1 prompts.
    - Tweak comments to better reflect what the alias or command behavior.
    - Fix minor typos and shameful spelling mistakes.

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Copyright (c) 2023 Robert LaRocca http://www.laroccx.com
+# Copyright (c) 2024 Robert LaRocca http://www.laroccx.com
 
 # Helpful Linux bash_aliases for sysadmins, developers and the forgetful.
 
 # Script version and release
-script_version='2.8.0'
+script_version='2.9.1'
 script_release='stable'  # options devel, beta, release, stable
 export BASH_ALIASES_VERSION="$script_version-$script_release"
 
@@ -17,7 +17,7 @@ set_emoji_ps1_prompt() {
 		PS1="☕ $PS1_ORIG"
 	elif [[ $USER = 'root' ]]; then
 		# Emoji for root
-		PS1="🐳 $PS1_ORIG"
+		PS1="🧀 $PS1_ORIG"
 	elif [[ $USER = 'user1' ]]; then
 		# Emoji for user1
 		PS1="🦄 $PS1_ORIG"
@@ -26,10 +26,109 @@ set_emoji_ps1_prompt() {
 		PS1="🩻 $PS1_ORIG"
 	elif [[ $USER = 'user3' ]]; then
 		# Emoji for user3
-		PS1="🗿 $PS1_ORIG"
+		PS1="🧟 $PS1_ORIG"
 	fi
 }
 set_emoji_ps1_prompt
+
+# User options for bash_aliases to show aliases, commands, help and version.
+bash_aliases() {
+	show_help_message() {
+		cat <<-EOF_XYZ
+		Usage: bash_aliases | alias | command [OPTION] [PARAMETER]...
+		Helpful Linux bash_aliases for sysadmins, developers and the forgetful.
+
+		This bash_aliases script by default (without an option) will only return
+		the text 'OK'. The commands section displays aliases and functions that
+		may be executed without the bash_aliases prefix, just like any other
+		installed Linux program or builtin tools and utilities.
+
+		Aliases:
+		kubectl - alias to prevent microk8s conflicts with existing (kubectl) packages
+
+		scp-passwd -  alias to prevent secure copy (scp) pubkey authentication
+		sftp-passwd - alias to prevent secure file transfer (sftp) pubkey authentication
+		ssh-passwd - alias to prevent secure remote login (ssh) pubkey authentication
+
+		speedtest - alias for Ookla (speedtest-cli) command with minimal output
+
+		wsl - alias for Windows PowerShell (wsl.exe) command
+		wslg - alias for Windows PowerShell (wslg.exe) command
+
+		Commands:
+		adpasswd - change your Active Directory domain user password
+
+		caffeinate - prevent the system suspend and hibernation timer (keep awake)
+		decaffeinate - restore the default suspend and hibernation timer (allow sleep)
+
+		open - open or edit file using the default GNOME application
+
+		lvmdisplay - show the logical volume management storage details
+		lvms - show the logical volume management storage
+		lvmsnapshot - show example commands how to create lvm snapshots
+
+		mkpsk - generate a secure random 64 character pre-shared key
+		mkpw - generate a secure ambiguous random 14 character password
+		mksecret - generate a secure random 512 character LUKS secret
+
+		test-port - check network port and try to connect with telnet
+		test-website - check website availability and display headers
+
+		wifi-power - toggle wireless network power management
+
+		Options:
+		version - show version information
+		help - show this help message
+
+		Exit Status:
+		0 - ok
+		1 - minor issue
+		2 - serious error
+
+		Copyright (c) $(date +%Y) Robert LaRocca, https://www.laroccx.com
+		License: The MIT License (MIT)
+		Source: https://github.com/robertlarocca/helpful-linux-bash-scripts
+
+		See bash(1) dash(1) man(1) nologin(8) apt(8) and os-release(5) for additional
+		information and to provide insight how this script works.
+		EOF_XYZ
+	}
+
+	show_version_information() {
+		cat <<-EOF_XYZ
+		bash_aliases $script_version-$script_release
+		Copyright (c) $(date +%Y) Robert LaRocca, https://www.laroccx.com
+		License: The MIT License (MIT)
+		Source: https://github.com/robertlarocca/helpful-linux-bash-scripts
+		EOF_XYZ
+	}
+
+	error_unrecognized_option() {
+		cat <<-EOF_XYZ
+		bash_aliases: unrecognized option '$2'
+		Try 'swupdate --help' for more information.
+		EOF_XYZ
+	}
+
+	case "$2" in
+	--help | help)
+		show_help_message
+		;;
+	--version | version)
+		show_version_information
+		;;
+	*)
+		# Default
+		if [[ -z "$2" ]]; then
+			echo "OK"
+			exit 0
+		else
+			error_unrecognized_option "$*"
+			exit 1
+		fi
+		;;
+	esac
+}
 
 # Windows Subsystem for Linux (WSL2) specific variables and aliases.
 # Use the $NTUSER variable just like $USER from witin Linux.
@@ -58,9 +157,10 @@ adpasswd() {
 	local domain_realm="EXAMPLE"
 	local domain_name="example.com"
 
-	# Set the default domain controller IP address or FQDN.
-	# NOTE: May also use $domain_name variable for best avaiable domain controller.
-	local domain_controller="192.168.99.21"
+	# Set the default domain controller IPv4 address or FQDN.
+	# NOTE: May also use the $domain_name variable for best available domain controller.
+	# local domain_controller="192.168.99.21"
+	local domain_controller="$domain_name"
 
 	# The 'samba-common-bin' packaged must be installed.
 	if [[ -x "$(which smbpasswd)" ]]; then
@@ -77,8 +177,6 @@ adpasswd() {
 }
 
 # Clear the current shell history after executing clean command.
-# You can checkout and download from the clean-command-history
-# repository here: https://github.com/robertlarocca/clean-command-history
 alias clean="$(which clean) $* && history -c"
 
 # Single command to disable or off the caffeinate script.
@@ -90,7 +188,7 @@ alias kubectl="microk8s kubectl"
 # Similar to the macOS 'open' command.
 alias open="$(which xdg-open)"
 
-# Prevent pubkey authentication with ssh and scp commands.
+# Prevent pubkey authentication with OpenSSH related commands.
 alias scp-passwd="scp -o PreferredAuthentications=password -o PubkeyAuthentication=no"
 alias sftp-passwd="sftp -o PreferredAuthentications=password -o PubkeyAuthentication=no"
 alias ssh-passwd="ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no"
@@ -114,7 +212,7 @@ lvmsnapshot() {
 	 lvcreate -L 25%ORIGIN --snapshot --name snapshot_1 /dev/ubuntu/root
 	 lvcreate -L 16G -s -n snapshot_2 /dev/ubuntu/home
 
-	See lvcreate(8) for additonal information.
+	See lvcreate(8) for additional information.
 	EOF_XYZ
 }
 
