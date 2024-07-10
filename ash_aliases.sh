294b7e555fcc3ba45f85a95b008134d3519645fa
Robert LaRocca <29333852+robertlarocca@users.noreply.github.com>
2024-07-10 09:58:06 -0400

    Remove `-openwrt` tag from the ASH_ALIASES_VERSION variable.

--- ash_aliases.sh
+++ ash_aliases.sh
@@ -5,7 +5,7 @@
 # Script version and release
 script_version='4.0.0'
 script_release='devel'  # options devel, beta, release, stable
-export ASH_ALIASES_VERSION="$script_version-$script_release-openwrt"
+export ASH_ALIASES_VERSION="$script_version-$script_release"
 
 PATH="$PATH:/usr/local/sbin:/usr/local/bin"
 
@@ -17,6 +17,7 @@ clean() {
 	/usr/local/bin/clean "$@"
 	if [[ "$SHELL" == "/bin/ash" ]]; then
 		echo "WARN: Cannot purge ash (busybox) history buffer." 2>&1
+		# history -c || history -p
 	elif [[ "$SHELL" == "/bin/bash" ]]; then
 		history -c
 	elif [[ "$SHELL" == "/bin/zsh" ]]; then
