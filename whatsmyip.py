7eb23f7bd7732e1d13547bdf84c3fd5577399a89
Robert LaRocca <29333852+robertlarocca@users.noreply.github.com>
2024-12-18 12:30:04 -0500

    Update whatsmyip.py
    - Minor bug fixes
    - Reorganize source code

--- whatsmyip.py
+++ whatsmyip.py
@@ -31,9 +31,9 @@ parser = argparse.ArgumentParser(
     add_help=True
 )
 parser.add_argument("-v", "--version", help="show version and exit", action="store_true")
-parser.add_argument("-a", "--all", help="IPv4 and IPv6 addresses", action="store_true")
 parser.add_argument("-4", "--ipv4", help="IPv4 address", action="store_true")
 parser.add_argument("-6", "--ipv6", help="IPv6 address", action="store_true")
+parser.add_argument("--all", help="all metadata", action="store_true")
 parser.add_argument("--country", help="country name", action="store_true")
 parser.add_argument("--iso", help="country ISO code", action="store_true")
 parser.add_argument("--region", help="region (aka state) name", action="store_true")
@@ -52,59 +52,79 @@ def show_version():
     sys.exit(0)
 
 
-def show_ips():
+def show_ip(proto):
     """show_ip"""
-    inet4 = os.popen(
-        f"curl -A {SCRIPT_NAME}/{SCRIPT_VERSION} -s4 {IP_SERVICE}/ip"
-    ).read().rstrip()
+    if os.name == "posix":
+        i = os.popen(
+            f"curl -A {SCRIPT_NAME}/{SCRIPT_VERSION} -s{proto} {IP_SERVICE}/ip"
+        ).read().rstrip()
+    else:
+        i = os.popen(
+            f"curl.exe -A {SCRIPT_NAME}/{SCRIPT_VERSION} -s{proto} {IP_SERVICE}/ip"
+        ).read().rstrip()
 
-    inet6 = os.popen(
-        f"curl -A {SCRIPT_NAME}/{SCRIPT_VERSION} -s6 {IP_SERVICE}/ip"
-    ).read().rstrip()
-
-    if inet4:
-        if args.ipv4 or args.all:
-            print("IPv4: " + inet4)
-    if inet6:
-        if args.ipv6 or args.all:
-            print("IPv6: " + inet6)
+    if i is not None:
+        if proto in ("4", "6"):
+            print(f"IP: {i}")
+        else:
+            print(f"IP: {i}")
+        show_metadata(proto)
 
 
-def show_metadata():
+def show_metadata(proto):
     """show_metadata"""
-    inet = os.popen(
-        f"curl -A {SCRIPT_NAME}/{SCRIPT_VERSION} -s {IP_SERVICE}/json"
-    ).read()
-    metadata = json.loads(inet)
+    if os.name == "posix":
+        j = os.popen(
+            f"curl -A {SCRIPT_NAME}/{SCRIPT_VERSION} -s{proto} {IP_SERVICE}/json"
+        ).read()
+        m = json.loads(j)
+    else:
+        j = os.popen(
+            f"curl.exe -A {SCRIPT_NAME}/{SCRIPT_VERSION} -s{proto} {IP_SERVICE}/json"
+        ).read()
+        m = json.loads(j)
 
-    if inet and metadata:
+    if j and m is not None:
+        if args.all:
+            print("Country: " + m["country"])
+            print("ISO: " + m["country_iso"])
+            print("Region: " + m["region_name"])
+            print("City: " + m["city"])
+            print("ZIP: " + m["zip_code"])
+            print("TZ: " + m["time_zone"])
+            print("ASN: " + m["asn"])
+            print("ISP: " + m["asn_org"])
         if args.country:
-            print("Country: " + metadata["country"])
+            print("Country: " + m["country"])
         if args.iso:
-            print("ISO: " + metadata["country_iso"])
+            print("ISO: " + m["country_iso"])
         if args.region:
-            print("Region: " + metadata["region_name"])
+            print("Region: " + m["region_name"])
         if args.city:
-            print("City: " + metadata["city"])
+            print("City: " + m["city"])
         if args.zip:
-            print("ZIP: " + metadata["zip_code"])
+            print("ZIP: " + m["zip_code"])
         if args.timezone:
-            print("TZ: " + metadata["time_zone"])
+            print("TZ: " + m["time_zone"])
         if args.asn:
-            print("ASN: " + metadata["asn"])
+            print("ASN: " + m["asn"])
         if args.isp:
-            print("ISP: " + metadata["asn_org"])
+            print("ISP: " + m["asn_org"])
         if args.json:
-            print(inet)
+            print(j)
 
 
 def main():
     """main"""
     if args.version:
         show_version()
+    if args.ipv4:
+        show_ip("4")
+    elif args.ipv6:
+        show_ip("6")
     else:
-        show_ips()
-        show_metadata()
+        show_ip("4")
+        show_ip("6")
 
 
 if __name__ == "__main__":
